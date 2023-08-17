2bdb26d57f7d14e05bc9f502a1abdb3fcf33bcdc
limhokyun <lim@nsmarts.co.kr>
2023-08-17 10:20:43 +0900

    first commit

---
+++ src/app/materials/materials.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatRippleModule} from '@angular/material/core';
+
+@NgModule({
+  exports: [
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatRippleModule
+  ]
+})
+export class MaterialsModule { }
